0f4109cfd2a38ea5ae9473d85acd581e12cf64a2
matti <melonmanchan@gmail.com>
2015-05-26 16:13:29 +0300

    Add sort-of workable scale to dragging

--- src/scripts/mixins/draggable.js
+++ src/scripts/mixins/draggable.js
@@ -15,7 +15,8 @@ export default {
 			containment: true
 		});
 		this.draggable.on('dragStart', (draggable, event) => {
-			this.setState({ isDragging: true });
+			this.setState({ isDragging: true,
+			startPos: {x:this.draggable.position.x,y:this.draggable.position.y} });
 			return event.stopPropagation();
 		});
 		this.draggable.on('dragEnd', () => {
@@ -23,17 +24,16 @@ export default {
 		});
 
 		this.draggable.on('dragMove', (event, pointer, moveVector) => {
-			console.clear();
-			console.log("Scale: " + this.state.moveScale);
-			console.log("Old x: " + this.draggable.position.x);
-			console.log("Old y: " + this.draggable.position.y);
+			console.log(this.state.moveScale);
 
-			this.draggable.position.x = this.draggable.position.x - (pointer.pageX / this.state.moveScale);
-			this.draggable.position.y = this.draggable.position.y - (pointer.pageY / this.state.moveScale);
-			console.log("New x: " + this.draggable.position.x);
-			console.log("New y: " + this.draggable.position.y);
-			console.log("Page x: " + pointer.pageX);
-			console.log("Page y: " + pointer.pageY);
+			let changeX = this.draggable.position.x - this.state.startPos.x;
+			let changeY = this.draggable.position.y - this.state.startPos.y;
+
+			let newX = this.state.startPos.x + changeX /this.state.moveScale;
+			let newY = this.state.startPos.y + changeY /this.state.moveScale;
+
+			this.draggable.position.x = newX;
+			this.draggable.position.y = newY;
 
 		});
 	},
